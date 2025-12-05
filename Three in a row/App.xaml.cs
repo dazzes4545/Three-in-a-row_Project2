bb6cd50dceef518086cbfa5f474eb15305361ca4
dazzes <dazzes7619@gmail.com>
2025-12-05 21:46:45 +0300

    Add project files.

---
+++ Three in a row/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Three_in_a_row
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
